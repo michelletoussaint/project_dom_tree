1f3b17413f459a922dae89f8a545a4f011612bea
michelletoussaint <toussaint.michelle@gmail.com>
2015-07-09 19:14:32 -0400

    Working on reading the file

--- parser.rb
+++ parser.rb
@@ -10,13 +10,24 @@ class Parse
 
   end
 
+  def read_doc(string)
+    stack = []
+    regex = /<\w.*?>/
+
+    first_tag = string.match(regex)
+    stack << first_tag
+
+    string = string [first_tag.length...-1]
+
+
+
+  end
+
 
   def parse_tag(string)
     @n = Node.new
     @n.name = string.match(/<(\w*?)[\s|>]/).captures.first
-    #
-    #<div> or <div class="foo"> 
-    #/<(.*?)\s||<(.*?)>/
+
     @n.text = string.match(/>(.*?)<\//).captures.first
     @n.classes = string.match(/class="(.*?)"/).captures.first.split(" ")
     @n.id = string.match(/id="(.*?)"/).captures.first
@@ -27,5 +38,11 @@ end
 
 p = Parse.new
 string = '<p class="foo bar class_2" id="baz" >Hello</p>'
-p.parse_tag(string)
-puts p.n
+string2 = "<html> <head> </head> </html>"
+
+p.read_doc(string2)
+p.read_doc(string)
+
+
+
+
