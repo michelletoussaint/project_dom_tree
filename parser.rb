36ca7bd04b7491b829c7c3fa8a05c57705157597
Garrett <garrettweinert@gmail.com>
2015-07-09 15:16:28 -0700

    psudocode

--- parser.rb
+++ parser.rb
@@ -13,7 +13,10 @@ class Parse
 
   def parse_tag(string)
     @n = Node.new
-    @n.name = string.match(/<(.*?)\s/).captures.first
+    @n.name = string.match(/<(\w*?)[\s|>]/).captures.first
+    #
+    #<div> or <div class="foo"> 
+    #/<(.*?)\s||<(.*?)>/
     @n.text = string.match(/>(.*?)<\//).captures.first
     @n.classes = string.match(/class="(.*?)"/).captures.first.split(" ")
     @n.id = string.match(/id="(.*?)"/).captures.first
