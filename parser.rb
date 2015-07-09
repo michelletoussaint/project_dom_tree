2c812c48dc6c89b9e5fb8437066b7516213e4a5c
michelletoussaint <toussaint.michelle@gmail.com>
2015-07-09 14:51:17 -0400

    Working on regex

--- parser.rb
+++ parser.rb
@@ -3,15 +3,26 @@ Node = Struct.new(:name, :text, :classes, :id, :children, :parent)
 
 class Parse
 
-  def initialize()
+  attr_reader :n
+
+  def initialize
+
+
   end
 
 
   def parse_tag(string)
+    @n = Node.new
+    @n.name = string.match(/<(.*?)\s/).captures.first
+    @n.text = string.match(/>(.*?)<\//).captures.first
+    @n.classes = string.match(/class="(.*?)"/).captures.first.split(" ")
+    @n.id = string.match(/id="(.*?)"/).captures.first
+
   end
 
 end
 
-string = "<p class = 'foo bar' id='baz' >Hello</p>"
-regex = /p/
-our 
+p = Parse.new
+string = '<p class="foo bar class_2" id="baz" >Hello</p>'
+p.parse_tag(string)
+puts p.n
