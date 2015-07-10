bd69bba6c444ecbd5cce03031dc38b05a29258ac
Garrett <garrettweinert@gmail.com>
2015-07-10 15:08:39 -0700

    working on rebuild tree

--- rebuild_html.rb
+++ rebuild_html.rb
@@ -1,38 +1,64 @@
+require_relative 'parser.rb'
+
 class Rebuild 
 
   def initialize(tree)
     @tree = tree
+    rebuild(@tree.root)
   end
 
-  def rebuild
-    node = @tree.root
+  def rebuild(node)
+    # node = node
     html = ""
     stack = []
     stack << node
-    close_tag_array = []
 
-
+    sibling_counter = 1
     until stack.empty? 
-      
+      puts "stack: #{stack.inspect} and #{stack.length}"
+
       node = stack.shift
+      # puts node.inspect
+      puts "node: #{node.name}\n\n"
+      sibling_counter -= 1
+
+      puts sibling_counter
 
       html << create_opening_tag(node)
-      close_tag_array << node
       
       if !node.children.empty? #has children
         
-        (children.length-1).downto(0) do |index|        
+        (node.children.length-1).downto(0) do |index|        
           stack.unshift(node.children[index])
+          sibling_counter += 1
         end
 
       else
+        print "html: #{html} and sibCount = #{sibling_counter}\n\n\n\n"
 
         html << create_closing_tag(node)
-        close_tag_array.pop
+        # sibling_counter -= 1
+        print "html: #{html} and sibCount = #{sibling_counter}\n\n\n\n"
 
+        if sibling_counter < 2  && sibling_counter>0
+          # add create_closing_tag to node.parent
+          print " Printing closing tag "
+          html << create_closing_tag(node.parent)
+        end
+        if sibling_counter <1
+          print " stack: #{stack.inspect}  "
+          print " node: #{node.name}   "
+          node = node.parent
+          until node.parent == nil
+            print " Backing out of node "
+            node = node.parent
+            html << create_closing_tag(node)
+          end
+        end
+        # end
       end
     end
-
+    print "final html: #{html}\n\n\n"
   end
 
   def create_opening_tag(node)
@@ -42,24 +68,24 @@ class Rebuild
     string << "<" + node.name
 
     unless node.classes == nil
-        string <<' class="'
+        string << ' class="'
       node.classes.each do |c|
-        string << c +" "
+        string << c + " "
       end
       string << '"'
     end
 
     unless node.id == nil
-      string << ' id="' + node.id +'"'
+      string << ' id="' + node.id + '"'
     end
 
-    string << ">"
+    string << "> "
 
   end
 
   def create_closing_tag(node)
 
-    string = "</" + node.name + ">"
+    string = "</" + node.name + "> "
   
   end
 
