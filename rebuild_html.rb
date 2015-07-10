d660d41eb1f3b160a8b8955b0890fce5d522f5d8
Garrett <garrettweinert@gmail.com>
2015-07-10 16:12:47 -0700

    ul closing before last li

--- rebuild_html.rb
+++ rebuild_html.rb
@@ -6,7 +6,6 @@ class Rebuild
     @tree = tree
     rebuild(@tree.root)
   end
-
   def rebuild(node)
     # node = node
     html = ""
@@ -40,12 +39,11 @@ class Rebuild
         # sibling_counter -= 1
         print "html: #{html} and sibCount = #{sibling_counter}\n\n\n\n"
 
-        if sibling_counter < 2  && sibling_counter>0
+        if sibling_counter == 1 && stack.length 
           # add create_closing_tag to node.parent
           print " Printing closing tag "
           html << create_closing_tag(node.parent)
-        end
-        if sibling_counter <1
+        elsif sibling_counter == 0
           print " stack: #{stack.inspect}  "
           print " node: #{node.name}   "
           node = node.parent
@@ -60,6 +58,59 @@ class Rebuild
     end
     print "final html: #{html}\n\n\n"
   end
+
+  # def rebuild(node)
+  #   # node = node
+  #   html = ""
+  #   stack = []
+  #   stack << node
+
+  #   sibling_counter = 1
+  #   until stack.empty? 
+  #     puts "stack: #{stack.inspect} and #{stack.length}"
+
+  #     node = stack.shift
+  #     # puts node.inspect
+  #     puts "node: #{node.name}\n\n"
+  #     sibling_counter -= 1
+
+  #     puts sibling_counter
+
+  #     html << create_opening_tag(node)
+      
+  #     if !node.children.empty? #has children
+        
+  #       (node.children.length-1).downto(0) do |index|        
+  #         stack.unshift(node.children[index])
+  #         sibling_counter += 1
+  #       end
+
+  #     else
+  #       print "html: #{html} and sibCount = #{sibling_counter}\n\n\n\n"
+
+  #       html << create_closing_tag(node)
+  #       # sibling_counter -= 1
+  #       print "html: #{html} and sibCount = #{sibling_counter}\n\n\n\n"
+
+  #       if sibling_counter == 1
+  #         # add create_closing_tag to node.parent
+  #         print " Printing closing tag "
+  #         html << create_closing_tag(node.parent)
+  #       elsif sibling_counter == 0
+  #         print " stack: #{stack.inspect}  "
+  #         print " node: #{node.name}   "
+  #         node = node.parent
+  #         until node.parent == nil
+  #           print " Backing out of node "
+  #           node = node.parent
+  #           html << create_closing_tag(node)
+  #         end
+  #       end
+  #       # end
+  #     end
+  #   end
+  #   print "final html: #{html}\n\n\n"
+  # end
 
   def create_opening_tag(node)
 
