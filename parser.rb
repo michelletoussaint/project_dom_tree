595c0e2163b1ef585dfe86bca1bd21099ad9ee84
michelletoussaint <toussaint.michelle@gmail.com>
2015-07-09 20:06:07 -0400

    Turned file into array

--- parser.rb
+++ parser.rb
@@ -7,29 +7,45 @@ class Parse
 
   def initialize
 
+    file = import_file
+    convert_to_array(file)
 
   end
 
-  # <html><head><title>
+  def import_file
 
+    file = File.open("test.html", "r")
+    content = file.read
+    file.close
+    puts content.inspect
+    content
 
-
-  def read_doc(string)
-    stack = []
-    regex = /<\w.*?>/
-    previous_text =""
-
-    first_tag = string.match(regex).to_s
-    stack << first_tag
-    puts stack.inspect
-    # string = string [first_tag.length...-1]
-    previous_text = string.match(regex).pre_match
-    string = string.match(regex).post_match
-    puts string
   end
 
 
+  def convert_to_array(string)
+    string_array = []
+    regex = /<.*?>/
 
+    until !string.match(regex)
+
+      tag = string.match(regex).to_s
+      # puts "Tag is #{tag}"
+      previous_text = string.match(regex).pre_match
+      # puts "Text before is #{previous_text}"
+      string = string.match(regex).post_match
+      # puts "Text after is #{string}"
+
+      unless previous_text.empty? || previous_text == " "
+        string_array << previous_text
+      end
+      string_array << tag
+
+    end
+
+    puts string_array.inspect
+
+  end
 
   def parse_tag(string)
     @n = Node.new
@@ -43,12 +59,12 @@ class Parse
 
 end
 
-p = Parse.new
-string = '<p class="foo bar class_2" id="baz" >Hello</p>'
-string2 = "<html> <head> </head> </html>"
+# p = Parse.new
+# string = '<p class="foo bar class_2" id="baz" >Hello</p>'
+# string2 = "<html> <head> <title> This is a test page </title> </head> </html>"
 
-p.read_doc(string2)
-p.read_doc(string)
+# # p.read_doc(string2)
+# p.read_doc(string)
 
 
 
